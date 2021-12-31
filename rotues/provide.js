3c74c2522b3a20ab1b160a9b873d09f946ae9a9b
nnaboon <srisawasdina@gmail.com>
2021-12-31 18:53:12 +0700

    feat: upload image in provide and request router

--- rotues/provide.js
+++ rotues/provide.js
@@ -1,3 +1,5 @@
+const Multer = require("multer");
+
 const {
   getProvides,
   getProvide,
@@ -5,19 +7,32 @@ const {
   updatedProvide,
   deletedProvide,
   addRequesterUser,
+  uploadImage,
+  getImage,
+  updateProvideSum,
 } = require("../controllers/provideController");
+
 const express = require("express");
 
 const router = express.Router();
+const multer = Multer({
+  storage: Multer.memoryStorage(),
+  limits: {
+    fileSize: 5 * 1024 * 1024,
+  },
+});
 
 router.get("/", getProvides);
 router.get("/:id", getProvide);
+router.get("image/:id", getImage);
 
 router.post("/", addProvide);
 router.post("/requester/:id", addRequesterUser);
+router.post("/upload", multer.single("img"), uploadImage);
 
 router.put("/:id", updatedProvide);
 router.put("/delete/:id", deletedProvide);
+router.put("/sum/:id", updateProvideSum);
 
 module.exports = {
   routes: router,
--- rotues/request.js
+++ rotues/request.js
@@ -1,3 +1,5 @@
+const Multer = require("multer");
+
 const {
   getRequests,
   getRequest,
@@ -7,17 +9,29 @@ const {
   updateProvidedStatus,
   updatedRequest,
   deletedRequest,
+  uploadImage,
+  getImage,
 } = require("../controllers/requestController");
+
 const express = require("express");
 
 const router = express.Router();
 
+const multer = Multer({
+  storage: Multer.memoryStorage(),
+  limits: {
+    fileSize: 5 * 1024 * 1024,
+  },
+});
+
 router.get("/", getRequests);
 router.get("/:id", getRequest);
+router.get("image/:id", getImage);
 
 router.post("/", addRequest);
 router.post("/requester/:id", addRequesterUserId);
 router.post("/provided/:id", addProvidedUserId);
+router.post("/upload", multer.single("img"), uploadImage);
 
 router.put("/:id", updatedRequest);
 router.put("/delete/:id", deletedRequest);
